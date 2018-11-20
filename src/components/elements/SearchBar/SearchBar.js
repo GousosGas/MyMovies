b5aaf3d3d8e31f9bb06b84196282375709455798
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-11-20 12:24:48 +0200

    main structure

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './SearchBar.css';
+import FontAwesome from 'react-fontawesome';
+
+class SearchBar extends Component {
+    state={
+
+    }
+
+    render() {
+        return (
+            <div>
+                SearchBar
+            </div>
+        );
+    }
+}
+
+
+export default SearchBar;
---
+++ src/components/elements/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css';
+
+const HeroImage = () => {
+    return (
+        <div className="loader"></div>
+    )
+};
+
+export default HeroImage;
